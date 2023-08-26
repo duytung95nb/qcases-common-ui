b31fd9883cc657e8dbeeb61dfd476b31ffc724b4
duytung95nb <duytung95nb@gmail.com>
2023-08-26 13:31:00 +0700

    Support full props for Custom button

--- src/ui-components/CommonButton.tsx
+++ src/ui-components/CommonButton.tsx
@@ -2,19 +2,17 @@ import React, { PropsWithChildren } from 'react';
 import { MDBBtn, MDBSpinner } from 'mdb-react-ui-kit';
 
 export const CustomButton: React.FC<{
-    color?: 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'light' | 'dark' | 'muted' | 'white' | 'info' | 'none' | 'link';
     loading: boolean;
-    type: "submit" | "reset" | "button" | undefined;
-} & PropsWithChildren> = (props) => {
-  const { color = 'primary', loading } = props;
+} & PropsWithChildren & typeof MDBBtn.defaultProps> = (props) => {
+  const { loading, children, ...btnProps } = props;
   return (
-    <MDBBtn type={props.type} color={color} disabled={loading}>
+    <MDBBtn {...btnProps} disabled={loading} >
       {loading ? (
         <MDBSpinner color={'light'} size="sm">
           <span className="visually-hidden">Loading...</span>
         </MDBSpinner>
       ) : (
-        props.children
+        children
       )}
     </MDBBtn>
   );
